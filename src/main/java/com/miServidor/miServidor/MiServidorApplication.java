81e5f08e3655a0226649cff2bc4701fa92fa74c3
wilkin547 <wilkintrinidad10@gmail.com>
2025-03-17 14:50:42 -0400

    primer commit

---
+++ src/main/java/com/miServidor/miServidor/MiServidorApplication.java
@@ -0,0 +1,13 @@
+package com.miServidor.miServidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiServidorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miServidor/miServidor/MiServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miServidor.miServidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
